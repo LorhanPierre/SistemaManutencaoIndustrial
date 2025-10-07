de4c10af8bd75cd524a48198eda64b2be2f58e33
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-10-06 21:54:48 -0300

    feat: Cria util para entrada de numeros

--- src/main/java/weg/manutencaoIndustrial/utils/Utils.java
+++ src/main/java/weg/manutencaoIndustrial/utils/Utils.java
@@ -1,4 +1,29 @@
 package weg.manutencaoIndustrial.utils;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static weg.manutencaoIndustrial.view.Mensagens.valorErrado;
+
 public class Utils {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static int inputNumero(){
+
+        boolean valorValido = false;
+
+        while(!valorValido){
+            try{
+                int numero = input.nextInt();
+                valorValido = true;
+            }catch(InputMismatchException error){
+                valorErrado();
+                input.nextLine();
+            }
+        }
+
+        return 0;
+    }
+
 }
--- src/main/java/weg/manutencaoIndustrial/view/Mensagens.java
+++ src/main/java/weg/manutencaoIndustrial/view/Mensagens.java
@@ -1,4 +1,10 @@
 package weg.manutencaoIndustrial.view;
 
 public class Mensagens {
+
+    public static void valorErrado(){
+        System.err.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.err.println("┃ Você digitou um valor incorreto, tente um número ┃");
+        System.err.println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+    }
 }
